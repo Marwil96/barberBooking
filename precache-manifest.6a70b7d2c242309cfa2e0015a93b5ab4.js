db2f867c4224b1c70ddad87510cc17ec43c4da7a
William_MAC <william_martinsson@hotmail.com>
2018-10-30 16:25:15 +0100

    Updates

---
+++ precache-manifest.6a70b7d2c242309cfa2e0015a93b5ab4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e9a6a666a1070c21eb3b85fc679a38f",
+    "url": "/barberBooking/static/media/potraitImage.7e9a6a66.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/barberBooking/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6b2a4d66acc7cd842adfb129dc292d5c",
+    "url": "/barberBooking/static/media/damklippning.6b2a4d66.svg"
+  },
+  {
+    "revision": "7f639a482999f310f2d4a5066b585295",
+    "url": "/barberBooking/static/media/blackArrow.7f639a48.svg"
+  },
+  {
+    "revision": "51574c43cb020809cb22b653bd408d5d",
+    "url": "/barberBooking/static/media/arrow.51574c43.svg"
+  },
+  {
+    "revision": "3e60c8b6ead3b4a6e82a",
+    "url": "/barberBooking/static/js/runtime~main.3e60c8b6.js"
+  },
+  {
+    "revision": "c8afba7bb0434c630e7d",
+    "url": "/barberBooking/static/js/main.c8afba7b.chunk.js"
+  },
+  {
+    "revision": "3ace4b6b37f3bce63314",
+    "url": "/barberBooking/static/js/1.3ace4b6b.chunk.js"
+  },
+  {
+    "revision": "c8afba7bb0434c630e7d",
+    "url": "/barberBooking/static/css/main.f9cf7a13.chunk.css"
+  },
+  {
+    "revision": "a521c9198bc525b8d002f4aeb8933b99",
+    "url": "/barberBooking/index.html"
+  }
+];
